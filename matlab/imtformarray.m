5b79d782d0dd48463641ad83453ba397bbdde2e9
Sunuk Park <sunukp0@gmail.com>
2023-10-08 20:39:28 -0700

    reorganizing

--- matlab/imtformarray.m
+++
@@ -1,7 +0,0 @@
-function timg=imtformarray(img, U, V)
-% Wrapper for matlab's tformarrray() function
-tmap_B = cat(3, U, V);
-resamp = makeresampler('cubic', 'fill');
-timg = tformarray(img, [], resamp, [2 1], [1 2], [], tmap_B, 0);
-end
-
